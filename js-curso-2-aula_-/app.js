e8ca48c90a22f113d3e81d63d04055bf6c916b7f
LuanaArimitsu <lmartinsarimitsu@gmail.com>
2024-06-24 14:02:15 -0300

    functions 2

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -1,15 +1,18 @@
+let numeroSecreto = geraeNumeroAleatorio();
+
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
 exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p','Escolha um número entre 1 e 100');
+exibirTextoNaTela('p','Escolha um número entre 1 e 10');
 
 function verificarChute() {
-    console.log('O botão foi clicado!');
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
 }
 
 function geraeNumeroAleatorio() {
-    parseInt(Math.random() * 10 + 1)
+    return parseInt(Math.random() * 10 + 1);
 }
